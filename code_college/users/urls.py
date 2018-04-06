85eeb6dc7caa2cea9104dc3c47a11887b7df86e1
joaorobson <joaorobsonmartins@gmail.com>
2018-04-06 00:32:02 -0300

    Add urls and serializers

--- code_college/users/urls.py
+++ code_college/users/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from dr_auth.views import RecoverView
+from rest_framework import routers
+from users.views import OrdinaryUserViewSet
 from rest_framework_jwt.views import (
     obtain_jwt_token,
     refresh_jwt_token
@@ -10,3 +12,8 @@ urlpatterns = [
     url(r'^refresh/$', refresh_jwt_token),
     url(r'^password/$', RecoverView.as_view(), name='password'),
 ]
+
+ROUTER = routers.DefaultRouter()
+ROUTER.register(r'^ordinary_user', OrdinaryUserViewSet)
+
+urlpatterns += ROUTER.urls
