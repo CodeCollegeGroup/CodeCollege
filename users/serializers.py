54ea5c39eb41693f1e2ee3c0ad8144e5ee92eb96
joaorobson <joaorobsonmartins@gmail.com>
2018-04-05 23:53:00 -0300

    Add ordinary user serializer

---
+++ users/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from models import OrdinaryUser
+
+class OrdinaryUserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = OrdinaryUser
+        fields = '__all__'
