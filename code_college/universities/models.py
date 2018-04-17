9a8b41091142f2dd46683bf8dd1fc85fb1f69fcf
mendesiasmin <mendesiasmin96@gmail.com>
2018-04-17 18:35:26 -0300

    Add university module

---
+++ code_college/universities/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class University(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Discipline(models.Model):
+    name = models.CharField(max_length=200)
+    university = models.ForeignKey(University,
+                     on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
