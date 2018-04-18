ca58190d5d14d5d73cb6ea28044c8f0f10214ede
mendesiasmin <mendesiasmin96@gmail.com>
2018-04-18 00:41:46 -0300

    Improve code style

--- code_college/projects/models.py
+++ code_college/projects/models.py
@@ -4,10 +4,13 @@ from users.models import User
 
 
 class Category(models.Model):
+
     name = models.CharField(max_length=200)
+
     description = models.CharField(max_length=500)
 
     class Meta:
+
         abstract = True
 
     def __str__(self):
@@ -15,23 +18,32 @@ class Category(models.Model):
 
 
 class ProjectCategory(Category):
+
     pass
 
 
 class DenouncementCategory(Category):
+
     pass
 
 
 class Project(models.Model):
+
     title = models.CharField(max_length=200)
+
     description = models.CharField(max_length=500)
+
     repository = models.CharField(max_length=300)
+
     deploy = models.CharField(max_length=300)
+
     discipline = models.ForeignKey(Discipline,
                                    on_delete=models.DO_NOTHING,
                                    null=True)
+
     # Como validar ao menos 1?
     contributors = models.ManyToManyField(User)
+
     categories = models.ManyToManyField(ProjectCategory)
 
     def __str__(self):
@@ -39,7 +51,9 @@ class Project(models.Model):
 
 
 class ProjectImage(models.Model):
+
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
     image = models.ImageField
 
     def __str__(self):
@@ -47,10 +61,15 @@ class ProjectImage(models.Model):
 
 
 class Comment(models.Model):
+
     message = models.CharField(max_length=400)
+
     image = models.ImageField
+
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
     answer_to = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
@@ -58,20 +77,26 @@ class Comment(models.Model):
 
 
 class Denouncement(models.Model):
+
     OPEN = 'OP'
     IN_ANALYSIS = 'AL'
     CLOSED = 'CL'
+
     STATUS = (
         (OPEN, 'open'),
         (IN_ANALYSIS, 'in analysis'),
         (CLOSED, 'closed'),
     )
+
     message = models.CharField(max_length=500)
+
     status = models.CharField(max_length=12, choices=STATUS, default=OPEN)
+
     category = models.ForeignKey(DenouncementCategory,
                                  on_delete=models.CASCADE)
 
     class Meta:
+
         abstract = True
 
     def __str__(self):
@@ -79,8 +104,10 @@ class Denouncement(models.Model):
 
 
 class ProjectDenouncement(Denouncement):
+
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
 
 class CommentDenouncement(Denouncement):
+
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
