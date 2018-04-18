60bf1b6330062a98161775aeab5a10a52f42b97b
mendesiasmin <mendesiasmin96@gmail.com>
2018-04-18 00:40:55 -0300

    Add university to user

--- code_college/users/models.py
+++ code_college/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from universities.models import University
 
 
 class OrdinaryUser(User, models.Model):
@@ -9,3 +10,7 @@ class OrdinaryUser(User, models.Model):
     college = models.CharField(max_length=80)
 
     college_registry = models.CharField(max_length=20)
+
+    project = models.ForeignKey(University,
+                                on_delete=models.CASCADE,
+                                null=True)
