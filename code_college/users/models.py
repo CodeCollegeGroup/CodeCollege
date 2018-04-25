cc30fcbd2bfec2cd2618ea48b7e7d2d4fb0d5f83
Vinicius Ferreira <vfbdelima@gmail.com>
2018-04-24 23:03:27 -0300

    Overrides create method of user viewset
    Co-authored-by: wallacybraz <wallacy.braz@live.com>
    Co-authored-by: danielmarques28 <danielmarques7@hotmail.com>

--- code_college/users/models.py
+++ code_college/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class OrdinaryUser(User, models.Model):
 
-    birthday = models.DateField()
+    birthday = models.DateField(blank=True, null=True)
 
     college = models.CharField(max_length=80)
 
