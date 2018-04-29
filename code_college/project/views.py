b4dfe59627d71a80798cd2b992e531a69fdfe75a
joaorobson <joaorobsonmartins@gmail.com>
2018-04-28 21:53:13 -0300

    Add serializers and views

--- code_college/project/views.py
+++ code_college/project/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project
+from .serializers import ProjectSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
