54c6c940b46ba5641dedeacafaa5d9f74a9aa2cf
mendesiasmin <mendesiasmin96@gmail.com>
2018-05-06 18:43:14 -0300

    Factory to Project

---
+++ code_college/projects/factories.py
@@ -0,0 +1,28 @@
+import factory
+import factory.fuzzy
+import datetime
+from . import models
+from users.factories import OrdinaryUserFactory
+from universities.factories import DisciplineFactory
+
+
+class ProjectFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = models.Project
+
+    title = factory.Faker('word')
+    description = factory.Faker('word')
+    repository = factory.LazyAttribute(lambda o: '%s@github.com' % o.title)
+    deploy = factory.LazyAttribute(lambda o: '%s@heroku.com' % o.title)
+    discipline = factory.SubFactory(DisciplineFactory)
+    recorder = factory.SubFactory(OrdinaryUserFactory)
+
+    @factory.post_generation
+    def contributors(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for contributor in extracted:
+                self.contributors.add(contributor)
