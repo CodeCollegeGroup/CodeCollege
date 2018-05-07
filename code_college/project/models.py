38fd9139485560f152213e353fe7bd19f28c8e96
Matias0422 <leguenta@hotmail.com>
2018-05-06 22:37:21 -0300

    Add Categories to project

--- code_college/project/models.py
+++ code_college/project/models.py
@@ -1,6 +1,65 @@
 from django.db import models
 from users.models import OrdinaryUser
 
+class ProjectCategory(models.Model):
+
+    description = models.CharField(max_length=500)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class App(ProjectCategory):
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(App, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
+class Game(ProjectCategory):
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(Game, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
+class Web(ProjectCategory):
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(Web, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
 
 class Project(models.Model):
 
@@ -14,9 +73,12 @@ class Project(models.Model):
 
     deploy = models.CharField(max_length=100)
 
-    colaborator = models.ManyToManyField(OrdinaryUser, related_name='contributed_projects')
+    colaborators = models.ManyToManyField(OrdinaryUser, related_name='contributed_projects')
 
-    university_class = models.CharField(max_length=150)
+    category = models.CharField(max_length=10)
+
+
+    # university_class =
 
 
 class ProjectProxy(Project):
@@ -27,6 +89,8 @@ class ProjectProxy(Project):
     def comment(self, project_id, user_id, answer_to, messege, image):
 
             Comment(project_id, user_id, answer_to, messege, image).save()
+
+
 
 
 
