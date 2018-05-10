389360c22739c3e0ebd1f781e42aac2199806008
Carlos Aragon <carloseraragon@gmail.com>
2018-05-10 18:48:39 -0300

    create denoucments and projects categories using singleton

--- code_college/categories/models.py
+++ code_college/categories/models.py
@@ -1,23 +1,99 @@
 from django.db import models
 
 
-class Category(models.Model):
+class ProjectCategory(models.Model):
 
-    name = models.CharField(max_length=200)
+    projects = models.ManyToManyField('projects.Project')
 
     description = models.CharField(max_length=500)
-
-    class Meta:
-        abstract = True
 
     def __str__(self):
         return self.name
 
-
-class ProjectCategory(Category):
-
-    projects = models.ManyToManyField('projects.Project')
+    class Meta:
+        abstract = True
 
 
-class DenouncementCategory(Category):
+class App(ProjectCategory):
+
+    def delete(self, *args, kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
+    def getName():
+        return "App"
+
+    def getDescription():
+        return "..."
+
+    def save(self, args, **kwargs):
+        self.pk = 1
+        super(App, self).save(args, kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Game(ProjectCategory):
+
+    def delete(self, *args, kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
+    def getName():
+        return "Game"
+
+    def getDescription():
+        return "..."
+
+    def save(self, args, **kwargs):
+        self.pk = 1
+        super(Game, self).save(args, kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Web(ProjectCategory):
+
+    def delete(self, *args, kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
+    def getName():
+        return "Web"
+
+    def getDescription():
+        return "..."
+
+    def save(self, args, **kwargs):
+        self.pk = 1
+        super(Web, self).save(args, kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class DenouncementCategory(models.Model):
+
     denounces = models.ManyToManyField('comments.Denouncement')
+
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
