f3068d322d09dca0b23917299820c322181f3cca
Matias0422 <leguenta@hotmail.com>
2018-05-10 18:29:20 -0300

    Apply Singleton on facade Profile

--- code_college/users/models.py
+++ code_college/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from comments.migrations import Rating
+
 
 class OrdinaryUser(User, models.Model):
 
@@ -8,19 +8,35 @@ class OrdinaryUser(User, models.Model):
 
     college_registry = models.CharField(max_length=20)
 
+
 class Profile(models.Model):
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(Profile, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
 
     def search_project_owner(self, user_id):
         user = User.objects.get(user_id)
-        projects = user.created_projects
+        projects = user.created_projects.all()
         return projects
 
     def search_project_contributes(self, user_id):
         user = User.objects.get(user_id)
-        contributors = user.contributed_projects
+        contributors = user.contributed_projects.all()
         return contributors
 
     def search_comments_owner(self, user_id):
         user = User.objects.get(user_id)
-        comments = user.author_comments
+        comments = user.author_comments.all()
         return comments
