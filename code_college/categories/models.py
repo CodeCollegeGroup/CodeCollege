18ed7de4e20205f71347be22c07347c8ede3cc71
Ícaro Pires <icaropsa@gmail.com>
2018-05-14 00:33:32 -0300

    Fix categories hierarchy and remove unnecessary classes

--- code_college/categories/models.py
+++ code_college/categories/models.py
@@ -1,121 +1,36 @@
 from django.db import models
 
 
-class ProjectCategory(models.Model):
-
-    projects = models.ManyToManyField('projects.Project')
+class Category(models.Model):
 
     name = models.CharField(max_length=200)
 
     description = models.CharField(max_length=500)
 
+    def delete(self, *args, kwargs):  # pylint: disable=arguments-differ
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, unused_created = cls.objects.get_or_create(pk=1)
+        return obj
+
     def __str__(self):
         return self.name
+
+    def save(self, args, **unused_kwargs):  # pylint: disable=arguments-differ
+        self.pk = 1
+        super(Category, self).save(args, unused_kwargs)
 
     class Meta:
         abstract = True
 
 
-class App(ProjectCategory):
+class ProjectCategory(models.Model):
 
-    def delete(self, *args, kwargs):
-        pass
-
-    @classmethod
-    def load(cls):
-        obj, created = cls.objects.get_or_create(pk=1)
-        return obj
-
-    def save(self, args, **kwargs):
-        self.pk = 1
-        super(App, self).save(args, kwargs)
-
-
-class Game(ProjectCategory):
-
-    def delete(self, *args, kwargs):
-        pass
-
-    @classmethod
-    def load(cls):
-        obj, created = cls.objects.get_or_create(pk=1)
-        return obj
-
-
-class Web(ProjectCategory):
-
-    def delete(self, *args, kwargs):
-        pass
-
-    @classmethod
-    def load(cls):
-        obj, created = cls.objects.get_or_create(pk=1)
-        return obj
-
-    def getName():
-        return "Web"
-
-    def getDescription():
-        return "..."
-
-    def save(self, args, **kwargs):
-        self.pk = 1
-        super(Web, self).save(args, kwargs)
+    projects = models.ManyToManyField('projects.Project')
 
 
 class DenouncementCategory(models.Model):
 
     denounces = models.ManyToManyField('comments.Denouncement')
-
-    name = models.CharField(max_length=200)
-
-    description = models.CharField(max_length=500)
-
-    def __str__(self):
-        return self.name
-
-
-class Spam(DenouncementCategory):
-
-    def delete(self, *args, kwargs):
-        pass
-
-    @classmethod
-    def load(cls):
-        obj, created = cls.objects.get_or_create(pk=1)
-        return obj
-
-    def save(self, args, **kwargs):
-        self.pk = 1
-        super(Web, self).save(args, kwargs)
-
-
-class InappropriateContent(DenouncementCategory):
-
-    def delete(self, *args, kwargs):
-        pass
-
-    @classmethod
-    def load(cls):
-        obj, created = cls.objects.get_or_create(pk=1)
-        return obj
-
-    def save(self, args, **kwargs):
-        self.pk = 1
-        super(Web, self).save(args, kwargs)
-
-
-class Piracy(DenouncementCategory):
-
-    def delete(self, *args, kwargs):
-        pass
-
-    @classmethod
-    def load(cls):
-        obj, created = cls.objects.get_or_create(pk=1)
-        return obj
-
-    def save(self, args, **kwargs):
-        self.pk = 1
-        super(Web, self).save(args, kwargs)
-
